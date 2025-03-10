59e0fa1bd8ba9c23cee4bbd1a2a9cdf51c11615a
Pavani Tiago <tiagopavanisalto@hotmail.com>
2025-03-10 00:46:34 -0300

    feat: new cors

--- internal/interfaces/http/middleware/middleware.go
+++ internal/interfaces/http/middleware/middleware.go
@@ -8,7 +8,7 @@ import (
 func SetupMiddlewares(app *fiber.App) {
 	// CORS configuration
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     "https://beta-intelligence.vercel.app, http://localhost:3000",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
